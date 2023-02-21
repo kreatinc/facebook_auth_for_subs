914b1b2ad64924f29594d148cd40c6909cb8c6a3
faissal20 <faissal@kreatinc.com>
2023-02-21 12:55:12 +0100

    update urls

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                     <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-1">
 
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                            <a href="http://localhost:8000/redirect?url=localhost:9000" class="">
+                            <a href="http://64.226.72.2:81/redirect?url=64.226.72.2" class="">
                                 
 
                                 <h2 class="mt-2 text-xl font-semibold text-gray-900 dark:text-white">Facebook Connect</h2>
