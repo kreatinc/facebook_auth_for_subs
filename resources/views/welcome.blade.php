55b8b3af54e11ad3e2197d97e40f8b65c3986b8b
faissal20 <faissal@kreatinc.com>
2023-02-21 15:56:23 +0100

    add some update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,8 +43,8 @@
 
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             @if(isset($name) && isset($email))
-                            <p class="mt-2 text-xl font-semibold text-gray-900 dark:text-white">
-                                Hello {{ $name }}, here's your email {{ $email }}.
+                            <p class="mt-2 text-md font-semibold text-gray-900 dark:text-white">
+                                Hello <span class="text-blue-400">{{ $name }}</span>, here's your email <span class="text-blue-400">{{ $email }}</span>. <br>
                                 We will be getting in touch with you soon.
                             </p>
                             @else
